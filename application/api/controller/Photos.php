a5dd684cda51fd94aa6398beed2af5b6db27172c
hiliqi <4780044@qq.com>
2019-02-02 11:42:02 +0800

    修改图片api

--- application/api/controller/Photos.php
+++ application/api/controller/Photos.php
@@ -12,18 +12,35 @@ namespace app\api\controller;
 use app\model\Photo;
 use think\Controller;
 use think\Request;
+use think\facade\App;
 
 class Photos extends Controller
 {
     public function save(Request $request){
         if ($request->isPost()){
             $data = $request->param();
+            $order = $data['order'];
             $pic = new Photo();
             $result = $pic->save([
                 'chapter_id' => $data['chapter_id'],
-                'order' => $data['order']
+                'order' => $order
             ]);
             if ($result){
+                $files = $request->file('image');
+                foreach($files as $file){
+                    $photo = new Photo();
+                    $photo->chapter_id = $data['chapter_id'];
+                    $photo->order = $order;
+                    $result = $photo->save();
+                    if ($result){
+                        $dir = App::getRootPath() . 'public/static/upload/book/'.$data['book_id'].'/'.$data['chapter_id'];
+                        if (!file_exists($dir)){
+                            mkdir($dir,0777,true);
+                        }
+                        $file->validate(['size'=>2048000,'ext'=>'jpg,png,gif'])->move($dir,$photo->id.'.jpg');
+                    }
+                    $order++;
+                }
                 return json([
                     'success' => 1,
                     'pic_id' => $pic->id,
